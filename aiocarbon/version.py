b0656a05b8489a2c31d3979b34717b44327524c5
Dmitry Orlov <me@mosquito.su>
2019-03-28 18:42:49 +0300

    [fix] bump

--- aiocarbon/version.py
+++ aiocarbon/version.py
@@ -9,7 +9,7 @@ package_license = "Apache 2"
 
 team_email = 'me@mosquito.su'
 
-version_info = (0, 14, 1)
+version_info = (0, 15, 0)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
